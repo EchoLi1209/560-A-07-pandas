bf1073407dc67d261607298a4258955f9713113e
Echo1209 <echo1209@ad.unc.edu>
2024-11-23 00:01:55 -0500

    final

--- roster.py
+++ roster.py
@@ -6,9 +6,14 @@ player = {"Last Name" : ["Claude", "Brown", "Hawkins", "Davis", "Tyson","Davis",
           "Trimble", "Powell", "Jackson", "Washington"],
           "First Name" : ["Ty","James","Elliot","RJ","Cade","Elijah","Seth",
           "Drack","Ian","Jalen"],
-          "Height" : [67,610,61,60,67,63,63,66,64,610],
+          "Height" : [79,82,73,72,79,75,75,78,76,82],
           "Weight" : [230,215,180,180,200,215,195,195,190,235]
           }
 data = pd.DataFrame(player)
+
+# bmi = weight in KG / height in meters squared
+data["bmi"] = round((data["Weight"]/2.205)/((data["Height"]/39.37)**2),2)
+
 print(data)
 
+data.to_csv("bmi.csv")
