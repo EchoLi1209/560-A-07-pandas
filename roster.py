49f08c8887bfcb301cadfecaf38344530218a175
Echo1209 <echo1209@ad.unc.edu>
2024-11-22 23:47:17 -0500

    no roster

--- roster.py
+++ roster.py
@@ -1,13 +1,14 @@
 # https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 
-roster = ["Claude", "Brown", "Hawkins", "Davis", "Tyson", "Davis", 
-          "Trimble", "Powell", "Jackson", "Washington"]
-player = {"Last Name" : roster,
+
+player = {"Last Name" : ["Claude", "Brown", "Hawkins", "Davis", "Tyson","Davis",
+          "Trimble", "Powell", "Jackson", "Washington"],
           "First Name" : ["Ty","James","Elliot","RJ","Cade","Elijah","Seth",
           "Drack","Ian","Jalen"],
           "Height" : [67,610,61,60,67,63,63,66,64,610],
-          "Weight" : [230,215,180,180,200,215,195,195,190,235]}
+          "Weight" : [230,215,180,180,200,215,195,195,190,235]
+          }
 data = pd.DataFrame(player)
 print(data)
 
